fd4dc5f13390849071906e569c3bb111d39d3a8c
yanlifeng <1362520198@qq.com>
2022-03-26 14:22:19 +0800

    rebuild all round1, see more in code, only round3 514w, add round2(xxx/1000) 307w,

--- CodeCraft-2022.cpp
+++ CodeCraft-2022.cpp
@@ -15,7 +15,7 @@
 #include <assert.h>
 #define ll long long
 const bool is_debug=0;
-const bool is_local=0;
+const bool is_local=1;
 using namespace std;
 map<string,int>mp_users;
 string users_name[500];
@@ -43,6 +43,7 @@ void inputData(){
     string in_path;
     if(is_local){
         //in_path="/Users/ylf9811/Downloads/huaweicode2022/smallData/";
+        //in_path="/Users/ylf9811/data_45/data3_4/";
         in_path="/Users/ylf9811/data_45/data/";
     }else{
         in_path="/data/";
@@ -134,7 +135,6 @@ struct edge{
 int num,head[500],ord[500],Tn;
 vector<int>Co[500];
 int nosMaxVal[500];
-int myrandom (int i) { return std::rand()%i;}
 void addEdge(int from,int to,int val){
     e[num].v=to;
     e[num].t=val;
@@ -150,8 +150,9 @@ bool Bfs(){
         int now=Q.front();
         Q.pop();
         vector<int>Ei;
-        for(int i=head[now];i!=-1;i=e[i].pre)
+        for(int i=head[now];i!=-1;i=e[i].pre){
             Ei.push_back(i);
+        }
         random_shuffle ( Ei.begin(), Ei.end() );
         for(auto i:Ei){
             if(e[i].t>0&&ord[e[i].v]==-1){
@@ -179,18 +180,9 @@ ll Dfs(int now,ll nowflow){
     return totflow;
 }
 int nodes_tims[500];
-vector<int>nos_val;
-vector<int>us_val;
-const double nodesLow=0.8;
-const double nodesUp=0.95;
-double nodesNow=nodesLow;
-const double nodesScale=nodesUp-nodesLow;
-bool user_cover[500];
-int user_cover_all;
-int user_cover_val;
 double ruduFac=1.0;
 
-ll SolMaxFlow(int limm){
+ll SolMaxFlow(int limm,vector<int>us_val,vector<int>nos_val){
     num=0;
     for(int i=0;i<Tn;i++)head[i]=-1;
     for(int i=0;i<M;i++){
@@ -211,174 +203,152 @@ ll SolMaxFlow(int limm){
         addEdge(Tn-1,i+M+1,0);
     }
     ll res=0;
-    while(Bfs())
+    while(Bfs()){
         res+=Dfs(0,1e15);
+    }
     return res;
 }
 
-ll SolMaxFlowNoLimit(vector<int>users_val){
+ll SolMaxFlowNoLimit(vector<int>us_val,vector<int>nos_val){
     num=0;
     for(int i=0;i<Tn;i++)head[i]=-1;
     for(int i=0;i<M;i++){
-        addEdge(0,i+1,users_val[i]);
+        addEdge(0,i+1,us_val[i]);
         addEdge(i+1,0,0);
     }
     for(int i=0;i<M;i++){
         for(int j=0;j<N;j++){
             if(dis[i][j]<DIS){
-                addEdge(i+1,j+M+1,users_val[i]);
+                addEdge(i+1,j+M+1,us_val[i]);
                 addEdge(j+M+1,i+1,0);
             }
         }
     }
     for(int i=0;i<N;i++){
-        addEdge(i+M+1,Tn-1,nodes_val[i]);
+        addEdge(i+M+1,Tn-1,nos_val[i]);
         addEdge(Tn-1,i+M+1,0);
     }
     ll res=0;
-    while(Bfs())
+    while(Bfs()){
         res+=Dfs(0,1e15);
+    }
     return res;
 }
 
 
-vector<string>Sol(vector<int>users_val){
-    printf("\n\n============================================================================= \n\n");
-    //init
-    vector<string>ansPath;
-    Tn=N+M+2;
-    nos_val.clear();
-    us_val.clear();
-    for(int i=0;i<N;i++)
-        nos_val.push_back(nodes_val[i]);
-    for(int i=0;i<M;i++)
-        us_val.push_back(users_val[i]);
-    int nodesSum[500][500];
-    for(int i=0;i<M;i++)
-        for(int j=0;j<N;j++)
-            nodesSum[i][j]=0;
-    int nodeALim=N-ceil(N*nodesNow);
+int nodesSumGol[9000][40][200];
+vector<int>NG[9000];
+int Round1(int tid,vector<int>&us_val,vector<int>&nos_val){
     int Tlim=T-ceil(T*0.95);
-    nodesNow+=nodesScale/T;
-    printf("nodesNow %lf\n",nodesNow);
-    int useNodes=0;
     set<int>nodeSet;
-    for(int i=0;i<N;i++)
-        if(nodes_tims[i]<Tlim)
-            nodeSet.insert(i);
-
-    //=============================================================================
-    //round1
-    while(nodeSet.size()){
-        if(useNodes>=nodeALim)break;
-        int done=1;
+    for(int j=0;j<N;j++)
+        if(nodes_tims[j]<Tlim)
+            nodeSet.insert(j);
+    if(nodeSet.size()==0)return 0;
+    vector<pair<double,int>>tmpNodes;
+    ll totSumMax=0;
+    vector<ll>totSums;
+    for(auto j:nodeSet){
+        ll totSum=0;
         for(int i=0;i<M;i++)
-            if(us_val[i])done=0;
-        if(done)break;
-        vector<pair<double,int>>tmpNodes;
-        ll totSumMax=0;
-        vector<ll>totSums;
-        for(auto i:nodeSet){
-            ll totSum=0;
-            for(int j=0;j<M;j++)
-                if(dis[j][i]<DIS)
-                    totSum+=us_val[j];
-            totSum=min(totSum,1ll*nos_val[i]);
-            totSums.push_back(totSum);
-            totSumMax=max(totSumMax,totSum);
-        }
-        int ii=0;
-        for(auto i:nodeSet){
-            tmpNodes.push_back(make_pair(-(1.0*totSums[ii++]/totSumMax+ruduFac*(1.0*(N-rudu[i])/N)),i));
-        }
-        sort(tmpNodes.begin(),tmpNodes.end());
-        int goodi=tmpNodes[0].second;
-        if(-tmpNodes[0].first==ruduFac*(1.0*(N-rudu[goodi])/N))break;
-        nodeSet.erase(goodi);
-
-        useNodes++;
-        nodes_tims[goodi]++;
-        printf("this round get goodi %d, use %.4f\n",goodi,tmpNodes[0].first);
-        vector<pair<int,int>> tmpUserNode;
-        for (int i=0;i<M;i++) {
-            tmpUserNode.push_back(make_pair(chudu[i],i));
-        }
-        sort(tmpUserNode.begin(),tmpUserNode.end());
-        for(auto it:tmpUserNode){
-            int i=it.second;
-            if(dis[i][goodi]>=DIS)continue;
-            int toUse=min(us_val[i],nos_val[goodi]);
-            us_val[i]-=toUse;
-            nos_val[goodi]-=toUse;
-            nodesSum[i][goodi]+=toUse;
-        }
+            if(dis[i][j]<DIS)
+                totSum+=us_val[i];
+        totSum=min(totSum,1ll*nos_val[j]);
+        totSums.push_back(totSum);
+        totSumMax=max(totSumMax,totSum);
     }
-    printf("after round 1:\n");
-    for(int i=0;i<M;i++)
-        printf("%d ",us_val[i]);
-    printf("\n");
+    if(totSumMax==0)return 1;
+    int ii=0;
+    for(auto j:nodeSet){
+        tmpNodes.push_back(make_pair(-(1.0*totSums[ii++]/totSumMax+ruduFac*(1.0*(N-rudu[j])/N)),j));
+    }
+    sort(tmpNodes.begin(),tmpNodes.end());
+    int goodi=tmpNodes[0].second;
+    if(-tmpNodes[0].first==ruduFac*(1.0*(N-rudu[goodi])/N))return 1;
+    nodes_tims[goodi]++;
+    printf("this round get goodi %d, use %.4f\n",goodi,tmpNodes[0].first);
+    vector<pair<int,int>> tmpUserNode;
+    for (int i=0;i<M;i++) {
+        tmpUserNode.push_back(make_pair(chudu[i],i));
+    }
+    sort(tmpUserNode.begin(),tmpUserNode.end());
+    for(auto it:tmpUserNode){
+        int i=it.second;
+        if(dis[i][goodi]>=DIS)continue;
+        int toUse=min(us_val[i],nos_val[goodi]);
+        us_val[i]-=toUse;
+        nos_val[goodi]-=toUse;
+        nodesSumGol[tid][i][goodi]+=toUse;
+    }
+    return 2;
+}
 
+int Round2(int tid,vector<int>&us_val,vector<int>&nos_val){
 
-    //=============================================================================
-    //round2
+    //printf("us_val : ");
+    //for(auto it:us_val)printf("%d ",it);
+    //printf("\n");
+    //printf("nos_val : ");
+    //for(auto it:nos_val)printf("%d ",it);
+    //printf("\n");
     ll tar=0;
-    for(int i=0;i<M;i++)tar+=us_val[i];
+    for(int i=0;i<M;i++)
+        tar+=us_val[i];
+    if(tar==0)return 1;
     int nodesMax=0;
-    for(int i=0;i<M;i++)nodesMax=max(nodesMax,nos_val[i]);
+    for(int j=0;j<N;j++)
+        nodesMax=max(nodesMax,nos_val[j]);
     int anspos=-1;
-    int delt=max(1,nodesMax/2000);
+    int delt=max(1,nodesMax/1000);
     for(int limm=1;limm<=nodesMax;limm+=delt){
-        if(SolMaxFlow(limm)==tar){
+        if(SolMaxFlow(limm,us_val,nos_val)==tar){
             anspos=limm;
             break;
         }
     }
     assert(anspos!=-1);
-    ll res=SolMaxFlow(anspos);
+    ll res=SolMaxFlow(anspos,us_val,nos_val);
     //=============================================================================
     //round3
     if(res!=tar){
         printf("roud12 failed, now rerun MacFlow!\n");
-        SolMaxFlowNoLimit(users_val);
-        for(int i=0;i<M;i++)
-            for(int j=0;j<N;j++)
-                nodesSum[i][j]=0;
+        ll res=SolMaxFlowNoLimit(us_val,nos_val);
+        printf("round3 done, %lld %lld\n",tar,res);
+        assert(res==tar);
+        if(res!=tar)return 0;
     }
-    //=============================================================================
-    //output
-    int sumCost[500];
-    for(int i=0;i<N;i++)sumCost[i]=0;
     for(int u=0;u<M;u++){
         for(int i=head[u+1];i!=-1;i=e[i].pre){
             if(e[i^1].t>0){
-                nosMaxVal[e[i].v-1-M] = max(nosMaxVal[e[i].v-1-M],e[i^1].t);
-                nodesSum[u][e[i].v-1-M]+=e[i^1].t;
+                int uid=u;
+                int nid=e[i].v-1-M;
+                int toUse=e[i^1].t;
+                nosMaxVal[nid]=max(nosMaxVal[nid],toUse);
+                assert(toUse<=us_val[uid]);
+                assert(toUse<=nos_val[nid]);
+                nodesSumGol[tid][uid][nid]+=toUse;
+                us_val[uid]-=toUse;
+                nos_val[nid]-=toUse;
             }
         }
     }
-    for(int i=0;i<M;i++){
-        string pat=users_name[i]+":";
-        vector<string>tmpAns;
-        for(int j=0;j<N;j++)
-            if(nodesSum[i][j]){
-                tmpAns.push_back("<"+nodes_name[j]+","+to_string(nodesSum[i][j])+">");
-                sumCost[j]+=nodesSum[i][j];
-            }
-        if(tmpAns.size()){
-            for(int i=0;i<tmpAns.size()-1;i++){
-                pat+=tmpAns[i]+",";
-            }
-            pat+=tmpAns[tmpAns.size()-1];
-        }
-        ansPath.push_back(pat);
-    }
-    for(int i=0;i<N;i++)Co[i].push_back(sumCost[i]);
-    return ansPath;
+    return 1;
 }
 
 int main() {
+    //it seems that round2 is ok ?
+    //now the biggest problem in round1 is that 0.90~0.95 isn't reasonable
+    //may no limit is correct, but when to break is important
+    //a totally new method is that we don't process a day one time
+    //we just select the most user's demand day, and pick one "good point" update user's demand
+    //in this way, we don't care the limmit in round1 
+    //just pick one nodes point one time until all of them have been picked timsLim times
+    //then in round2, just do some greedy or run max_flow
+    //
+    //however, how to process no solution ?
     std::srand ( unsigned ( std::time(0) ) );
     inputData();
+    Tn=N+M+2;
     printf("M:%d,N:%d\n\n",M,N);
     printf("chu du:\n");
     for(int i=0;i<M;i++){
@@ -407,6 +377,47 @@ int main() {
     if(!outstrm){
         printf("open out file fail\n");
     }
+    for(int tt=0;tt<T;tt++)
+        for(int j=0;j<N;j++)
+            NG[tt].push_back(nodes_val[j]);
+
+    //============================================
+    //round1
+    vector<pair<int,vector<int>>>waitToProcess;
+    while(1){
+        vector<pair<ll,int>>Tis;
+        for(int tt=0;tt<T;tt++){
+            ll sum=0;
+            for(auto it:G[tt])sum+=it;
+            Tis.push_back(make_pair(-sum,tt));
+        }
+        sort(Tis.begin(),Tis.end());
+        if(Tis[0].first==0)break;
+        int nowBigDayId=Tis[0].second;
+        int res=Round1(nowBigDayId,G[nowBigDayId],NG[nowBigDayId]);
+        printf("this round res is %d\n",res);
+        //res==0 -> no good nodes can use
+        //res==1 -> still remain good nodes, but for nowBigDayId no use
+        //res==2 -> ok
+        if(res==0)break;
+        if(res==1){
+            vector<int>tmp; 
+            for(auto &it:G[nowBigDayId]){
+                tmp.push_back(it);
+                it=0;
+            }
+            waitToProcess.push_back(make_pair(nowBigDayId,tmp));
+        }
+    }
+
+    //==================================================
+    //round2
+    for(auto it:waitToProcess){
+        int tid=it.first;
+        auto tmp=it.second;
+        for(int i=0;i<M;i++)
+            G[tid][i]=tmp[i];
+    }
     vector<pair<ll,int>>Tis;
     for(int tt=0;tt<T;tt++){
         ll sum=0;
@@ -414,48 +425,62 @@ int main() {
         Tis.push_back(make_pair(-sum,tt));
     }
     sort(Tis.begin(),Tis.end());
-    vector<pair<int,vector<string>>>AAns;
     for(int tt=0;tt<T;tt++){
-        printf("day %d %d\n",tt,Tis[tt].second);
         int tid=Tis[tt].second;
-        vector<string>res=Sol(G[tid]);
-        AAns.push_back(make_pair(tid,res));
-        if(is_debug){
-            printf("---------------------------\n");
-            for(auto it:res)printf("%s\n",it.c_str());
-            printf("---------------------------\n");
-        }
+        int ok=1;
+        for(auto it:G[tid])if(it)ok=0;
+        if(ok==1)continue;
+        printf("round2 process day %d %d\n",tid,tt);
+        int res=Round2(tid,G[tid],NG[tid]);
+        assert(res==1);
     }
-    sort(AAns.begin(),AAns.end());
-    for(auto it:AAns){
-        auto res=it.second;
-        for(auto item:res){
-            outstrm<<item<<"\n";
+    //=====================================================
+    //output
+    for(int tt=0;tt<T;tt++){
+        int sumCost[500];
+        for(int j=0;j<N;j++)
+            sumCost[j]=0;
+        for(int i=0;i<M;i++){
+            string pat=users_name[i]+":";
+            vector<string>tmpAns;
+            for(int j=0;j<N;j++)
+                if(nodesSumGol[tt][i][j]){
+                    tmpAns.push_back("<"+nodes_name[j]+","+to_string(nodesSumGol[tt][i][j])+">");
+                    sumCost[j]+=nodesSumGol[tt][i][j];
+                }
+            if(tmpAns.size()){
+                for(int i=0;i<tmpAns.size()-1;i++){
+                    pat+=tmpAns[i]+",";
+                }
+                pat+=tmpAns[tmpAns.size()-1];
+            }
+            outstrm<<pat<<endl;
         }
+        for(int j=0;j<N;j++)Co[j].push_back(sumCost[j]);
     }
     outstrm.close();
-    if(is_local){
-        printf("%d\n",T);
-        printf("tims:\n");
-        for(int i=0;i<N;i++)printf("%d ",nodes_tims[i]);
-        printf("\n");
-        ll totCost=0;
-        int pos95=ceil(T*95/100.0);
-        printf("pos %d\n",pos95);
-        for(int i=0;i<N;i++){
-            printf("node name:%s, random use time:%d, rudu:%d: ",nodes_name[i].c_str(),nodes_tims[i],rudu[i]);
-            vector<pair<int,int>>tmpCo;
-            for(int t=0;t<Co[i].size();t++){
-                tmpCo.push_back(make_pair(Co[i][t],t));
-            }
-            sort(tmpCo.begin(),tmpCo.end());
-            for(auto it:tmpCo)printf("%d %d, ",it.first,it.second);
-            printf("\n");
-            totCost+=tmpCo[pos95-1].first;
-            printf("----------%d\n",tmpCo[pos95-1].first);
+    //========================================================
+    //local judge
+    printf("%d\n",T);
+    printf("tims:\n");
+    for(int j=0;j<N;j++)printf("%d ",nodes_tims[j]);
+    printf("\n");
+    ll totCost=0;
+    int pos95=ceil(T*95/100.0);
+    printf("pos %d\n",pos95);
+    for(int j=0;j<N;j++){
+        printf("node name:%s, random use time:%d, rudu:%d: ",nodes_name[j].c_str(),nodes_tims[j],rudu[j]);
+        vector<pair<int,int>>tmpCo;
+        for(int t=0;t<Co[j].size();t++){
+            tmpCo.push_back(make_pair(Co[j][t],t));
         }
-        printf("cost %lld\n",totCost);
-
+        sort(tmpCo.begin(),tmpCo.end());
+        for(auto it:tmpCo)printf("%d %d, ",it.first,it.second);
+        printf("\n");
+        totCost+=tmpCo[pos95-1].first;
+        printf("----------%d\n",tmpCo[pos95-1].first);
     }
+    printf("cost %lld\n",totCost);
+
     return 0;
 }
